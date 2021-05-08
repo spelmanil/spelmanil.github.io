ba33554b7f4c6624a4cfb7469c4dfe97d315c946
jvolcy <jerry.volcy@gmail.com>
2021-05-07 23:12:29 -0400

    index.html now accepts GDoc web link as search query parameter; the python script to generate index.html is added.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # spelmanil.github.io
 Spelman Innovation Lab
+
+Use the python script make_gdoc_html.py one time to create the index.html file.
+
+index.html by default references the 4 images in the img directory.  These are the images for the 'back' button, the logo in the header and the two images in the footer.  The filenames indicate which image is which.
+
+Pass a Google Doc weblink to the index.hmtl file via a URL search query to display the doc in the customized index.html webpage.
+
+For example, if this webpage is index.html, and you wish to view a Google doc at `https://docs.google.com/abcxyz123`, then construct the following URL:
+            `http://index.html?https://docs.google.com/abcxyz123`
+            
+Because `index.html` is the default html page, it can be omitted.  The following will also work: `spelmanil.github.io?https://docs.google.com/abcxyz123`.  This is the preferred method.
---
+++ make_gdoc_html.py
@@ -0,0 +1,142 @@
+#---------- define the html_template ----------
+html_template = '''
+<html>
+    <head>
+        <title>Innovation Lab Docs</title>
+    </head>
+    <style>
+
+    body {
+        background-color: {backgroundColor};
+        width:{bodyWidth};
+        min-width: 400px;
+        margin-left: auto;
+        margin-right:auto;
+        overflow: hidden;
+        margin-top: 1%;
+        margin-bottom: 1%;
+    }
+
+    #container {
+        background-color: {themeColor};
+        width:100%;
+        min-width:100%;
+        height:100%;
+        min-height:100%;
+        margin-left:auto;
+        margin-right:auto;
+        padding:0px;
+    }
+
+    #header {
+        height:10%;
+        min-height:10%;
+        padding:0px 30px 0px 30px;
+        margin-top:auto;
+        margin-bottom:auto;
+    }
+
+    #content {
+      width:100%;
+      min-width:100%;
+      /* 78% = 80% - top_margin - bottom_margin */
+      height:78%;
+      min-height:78%;
+      padding:0px;
+    }
+
+    #footer {
+        height:10%;
+        min-height:10%;
+        text-align: left;
+        margin-top:auto;
+        margin-bottom:auto;
+        padding:0px 30px 0px 30px;
+    }
+    
+    #footer img {
+      margin-top:2%; /* ~ of 2.5% */
+    }
+
+    </style>
+    <body>
+        <div id="container">
+            <div  id="header">
+                <a href="{backURL}"><img style="height:100%;" src="img/back.png" /></a><img style="height:100%;" src="img/logo.png"></img>
+            </div>
+
+            <!-- publish your Google doc to the web.  Then, pass the
+            weblink as a query parameter by appending it to the URL
+            for this html page, separated by a '?'.  For example, if
+            this webpage is index.html, and you wish to view a Google
+            doc at https://docs.google.com/abcxyz123, then construct
+            the following URL:
+            http://index.html?https://docs.google.com/abcxyz123 -->
+            
+            <script>
+            var GDocWebLink = '<embed id="content" src="' + window.location.search.substring(1) + '?embedded=true"></embed>';
+            window.document.write(GDocWebLink);
+            </script>
+            
+            <div id="footer">
+              <img style="float:left;height:90%;" src="img/lower_left_image.png"></img>
+              <img style="float:right;height:90%;" src="img/lower_right_image.png"></img>
+
+            </div>
+
+        </div>
+
+    </body>
+
+</html>
+
+'''
+
+INPUT_REQUIRED = 'INPUT REQUIRED'
+
+#---------- define the placeholders & defaults ----------
+placeholders = dict();
+
+placeholders['{backgroundColor}'] = '#eeeeee'
+placeholders['{themeColor}'] = '#2d76d8'
+placeholders['{backURL}'] = 'javascript:history.back()'
+#placeholders['{GDocWebLink}'] = INPUT_REQUIRED
+#placeholders['{GDocWebLink}'] = 'https://docs.google.com/document/d/e/2PACX-1vTwkcjKQmjb7W0U2Yq0zZVR9VRzVbM_kOgYdLyMnQFthEKDO-jiYaCF-ucA83yhfVE24oVaEnQhPnhR/pub'
+placeholders['{bodyWidth}'] = '80%'
+
+
+def updateHtmlParameter(placeholder):
+    prompt = 'Enter value for '+placeholder+' ['+placeholders[placeholder]+']: '
+    x = input(prompt)
+    if x!='':
+        #if the user entered a value, update the dictionary.  Otherwise, do nothing
+        placeholders[placeholder] = x.strip()
+    
+#---------- get placeholder parameters from user ----------
+for placeholder in placeholders:
+    if placeholders[placeholder] == INPUT_REQUIRED:
+        #loop until the user enters a value
+        while placeholders[placeholder] == INPUT_REQUIRED:
+            updateHtmlParameter(placeholder)
+    else:
+        updateHtmlParameter(placeholder)
+    
+    #upate the html template string
+    html_template = html_template.replace(placeholder, placeholders[placeholder])
+ 
+    
+#Get the name of the output ifle
+outFile = 'index.html'
+prompt = 'Enter output file name ['+outFile+']: '
+
+x = input(prompt)
+if x!='':
+    outFile = x.strip()
+
+outFile = open(outFile, 'wt')
+outFile.write(html_template)
+outFile.close()
+
+
+#----------  ----------
+#----------  ----------
